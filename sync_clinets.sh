b6028aa3f4900c738cf778cf275c553d17d2b503
mzohar <matanzr@gmail.com>
2018-06-13 20:56:39 +0300

    update sync and removed magnet when not existing

--- sync_clinets.sh
+++ sync_clinets.sh
@@ -4,7 +4,7 @@
 FROM=./led_control/incoming_images/
 # Remote Directory to retrieve. Files are retrieved recursively starting here. Hidden files are included.
 # Must be full path, don't use ~ shortcut.
-TO=dev/fugara_revelation/led_control/
+TO=dev/fugara_revelation/led_control/incoming_images/
 # Path to SSH ID file (private key)
 ID=~/.ssh/id_rsa
 # USERname to login as
@@ -15,9 +15,18 @@ FLAGS="-avh --delete"
 
 echo to remove password use: ssh-copy-id pi@rev4.local
 
-rsync $FLAGS --exclude='*.png' $FROM $USER@rev1.local:$TO
-rsync $FLAGS --exclude='*.png' $FROM $USER@rev2.local:$TO
-rsync $FLAGS --exclude='*.png' $FROM $USER@rev3.local:$TO
-rsync $FLAGS --exclude='*.png' $FROM $USER@rev4.local:$TO
-rsync $FLAGS --exclude='*.png' $FROM $USER@rev5.local:$TO
-rsync $FLAGS --exclude='*.png' $FROM $USER@rev6.local:$TO
+rsync $FLAGS --exclude='*.png' $FROM $USER@rev1.local:$TO &
+rsync $FLAGS --exclude='*.png' $FROM $USER@rev2.local:$TO &
+rsync $FLAGS --exclude='*.png' $FROM $USER@rev3.local:$TO &
+rsync $FLAGS --exclude='*.png' $FROM $USER@rev4.local:$TO &
+rsync $FLAGS --exclude='*.png' $FROM $USER@rev5.local:$TO &
+rsync $FLAGS --exclude='*.png' $FROM $USER@rev6.local:$TO &
+wait
+
+ssh pi@rev1.local 'cd dev/fugara_revelation && python firebase_sync.py extract' &
+ssh pi@rev2.local 'cd dev/fugara_revelation && python firebase_sync.py extract' &
+ssh pi@rev3.local 'cd dev/fugara_revelation && python firebase_sync.py extract' &
+ssh pi@rev4.local 'cd dev/fugara_revelation && python firebase_sync.py extract' &
+ssh pi@rev5.local 'cd dev/fugara_revelation && python firebase_sync.py extract' &
+ssh pi@rev6.local 'cd dev/fugara_revelation && python firebase_sync.py extract' & 
+wait
